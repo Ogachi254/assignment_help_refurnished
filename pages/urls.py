606f8ba2ec6c3075ecd755639099b097fc948e0d
Ogachi254 <dancanogachi1@gmail.com>
2023-05-20 23:52:39 +0300

    form functionality

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
-
-from .views import  HomePageView,  AboutPageView
-from .views import contact_view
+from django.urls import path, include
+from .views import HomePageView, AboutPageView, contact_view, ContactView, success_page
 
 urlpatterns = [
+    path('success/', success_page, name='success_page'),
+    path('contact/', ContactView.as_view(), name='save_contact'),
     path('contact/', contact_view.as_view(), name='contact'),
     path('about/', AboutPageView.as_view(), name='about'), 
     path('', HomePageView.as_view(), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),  # Include authentication URLs
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Contact
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -9,3 +12,24 @@ class AboutPageView(TemplateView):
 
 class contact_view(TemplateView):
     template_name = 'contact.html'
+
+class ContactView(LoginRequiredMixin, View):
+    login_url = '/accounts/login/'  # Specify the login URL
+
+    def get(self, request):
+        return render(request, 'contact.html')
+
+    def post(self, request):
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        contact = Contact(name=name, email=email, subject=subject, message=message)
+        contact.save()
+
+        # Redirect the user to the success page
+        return redirect('success_page')
+
+def success_page(request):
+    return render(request, 'success.html')
