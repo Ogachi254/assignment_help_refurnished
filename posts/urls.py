df97eeae332ec9bf9cdf5993f60d802e9f185dc2
Ogachi254 <dancanogachi1@gmail.com>
2023-05-26 21:59:46 +0300

    post file B

--- posts/urls.py
+++ posts/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
-    path('delete/<int:pk>/', login_required(views.post_delete), name='post_delete'),
     path('', login_required(views.post_list), name='post_list'),
     path('create/', login_required(views.post_create), name='post_create'),
     path('edit/<int:pk>/', login_required(views.post_edit), name='post_edit'),
+    path('delete/<int:pk>/', login_required(views.post_delete), name='post_delete'),
     path('success/', views.post_success, name='post_success'),
 ]
